eb4cf0ce7966244ba5f04f9d024bd37ae747e428
Aimene Amiour <aimeneamiour@gmail.com>
2018-12-24 16:30:58 +0100

    clclasse terrain 80%

--- terrain.h
+++ terrain.h
@@ -5,32 +5,45 @@
 #include "point.h"
 #include <vector>
 #include "cases.h"
+#include<string>
 #include "std.h"
 using namespace geom;
+
+namespace gestionRobotTerrain {
 class terrain
 {
 public:
 
-    terrain();
-    terrain(const point & position);
-    terrain()
+
+    terrain(const string& nomFichier);
+    terrain(const point & position, const string& nomFichier,const vector<vector<cases*>> terrainMatrice);
     ~terrain();
+
     int hauteur() const;
     int largeur() const;
-    void changerHauteur( int hauteur);
-    void changerLargeur( int largeur);
-    void litTerrain(const string& nomFichier);
+    const string& nomFichier() const;
+    const vector<vector<cases*>> terrainMatrice() const ;
+
+    vector<vector<cases*>> terrainMatriceModifieCase();
+
+
+    void chanegrNomFichier(const string& nomFichier);
+
+    bool litTerrain();
     void sauveTerrain() ;
-   string nomFichier() const;
-// accesseur et manipulateur terrain
-   void dessineTerrain(fenetre& fenetre);
-   void chanegrNomFichier(const string& nomFichier);
 
+
+    void dessineTerrain(fenetre& fenetre);
+
+    static const string repertoire = "terrain/";
 private:
-    point d_position
-    int largeur,hauteur;
-    vector<vector<cases*> > d_terrain;
-    string& d_nomFichier;
-};
+    void changeHauteur( int hauteur);
+    void changeLargeur( int largeur);
 
+    point d_position;
+    int d_largeur,d_hauteur;
+    vector<vector<cases*> > d_terrain;
+    string d_nomFichier;
+};
+}
 #endif // TERRAIN_H
