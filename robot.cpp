62ba1e79a0e6fe4a700f456ad353ae318165bfa3
Oumar Marega <oumarmarega18@gmail.com>
2018-12-26 15:38:03 +0100

    tourneGauche et Droite rectifie

--- robot.cpp
+++ robot.cpp
@@ -5,69 +5,69 @@ using gestionRobotTerrain::robot;
 
 namespace gestionRobotTerrain
 {
-    robot::robot(const point& position, int direction):d_position{position},d_direction{direction}
-    {}
-    int robot::direction() const
+robot::robot(const point& position, int direction):d_position{position},d_direction{direction}
+{}
+int robot::direction() const
+{
+    return d_direction;
+}
+void robot::changeDirection(int direction)
+{
+    d_direction = direction;
+}
+point robot::positionRobot() const
+{
+    return d_position;
+}
+void robot::changePositionRobot(const point& position)
+{
+    d_position = position;
+}
+void robot::tourneDroite()
+{
+    switch(d_direction)
     {
-       return d_direction;
+    case EST :
+        d_direction = SUD;
+        break;
+    case OUEST:
+        d_direction = NORD;
+        break;
+    case NORD :
+        d_direction = EST;
+        break;
+    case SUD :
+        d_direction = OUEST;
+        break;
     }
-    void robot::changeDirection(int direction)
-    {
-      d_direction = direction;
-    }
-    point robot::positionRobot() const
-    {
-        return d_position;
-    }
-     void robot::changePositionRobot(const point& position)
-    {
-       d_position = position;
-    }
-     void robot::tourneGauche()
-    {
-       switch(d_direction)
-       {
-       case EST :
-                 d_direction = SUD;
-                 break;
-       case OUEST:
-                 d_direction = NORD;
-                 break;
-       case NORD :
-                  d_direction = EST;
-                  break;
-       case SUD :
-                 d_direction = OUEST;
-                 break;
-       }
-    }
+}
 
-    void robot::tourneDroite()
+void robot::tourneGauche()
+{
+    switch(d_direction)
     {
-          switch(d_direction)
-       {
-       case EST :
-                 d_direction = NORD;
-                 break;
-       case OUEST:
-                 d_direction = SUD;
-                 break;
-       case NORD :
-                  d_direction = OUEST;
-                  break;
-       case SUD :
-                 d_direction = EST;
-                 break;
-       }
+    case EST :
+        d_direction = NORD;
+        break;
+    case OUEST:
+        d_direction = SUD;
+        break;
+    case NORD :
+        d_direction = OUEST;
+        break;
+    case SUD :
+        d_direction = EST;
+        break;
     }
-    bool robot::detecteObstacleDevant(const terrain& terrain)
-    {
+}
+bool robot::detecteObstacleDevant(const terrain& terrain)
+{
 
-    }
-    void robot::avanceCase(const terrain& terrain)
-    {
+}
+void robot::avanceCase(const terrain& terrain)
+{
 
-    }
+}
 
 
 
